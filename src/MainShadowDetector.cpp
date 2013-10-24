d493df60fb6ef90724ab26626e43276c5e8bae9b
Henrique Weber <iqueweber@gmail.com>
2013-10-24 01:02:39 -0300

    CMakeLists and empty .cpp files creates

---
+++ src/MainShadowDetector.cpp
@@ -0,0 +1,8 @@
+//load image and sends to the ShadowDetector class
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Loading image"<<endl;
+}
---
+++ src/ShadowDetector.cpp
@@ -0,0 +1,1 @@
+//class that detects shadow of a car
