a6a910cde5ec3d55446e0470c8a0cd798073a0a0
Henrique Weber <iqueweber@gmail.com>
2013-10-24 01:15:23 -0300

    opencv library working and opening picture

--- src/MainShadowDetector.cpp
+++ src/MainShadowDetector.cpp
@@ -1,8 +1,25 @@
 //load image and sends to the ShadowDetector class
 #include <iostream>
-using namespace std;
 
-int main()
+#include "MainShadowDetector.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
 {
-	cout<<"Loading image"<<endl;
+    Mat image;
+    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
+
+    if(! image.data )                              // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+
+    namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", image );                   // Show our image inside it.
+
+    waitKey(0);                                          // Wait for a keystroke in the window
+    return 0;
 }
