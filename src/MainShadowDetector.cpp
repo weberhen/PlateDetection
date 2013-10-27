c3e192b8bd300abdf0d0274861b6d640b306579c
Henrique Weber <iqueweber@gmail.com>
2013-10-27 11:01:36 -0200

    eliminated useless steps in main

--- src/MainShadowDetector.cpp
+++ src/MainShadowDetector.cpp
@@ -3,21 +3,21 @@
 int main(int argc, char** argv)
 {
 	Mat dst, src_gray;
-	structAsphaltInfo _structAsphaltInfo;
-	_structAsphaltInfo.porcentageAsphalt=0;
 
+	//open image
 	Mat src = imread(argv[1]);
 	if(!src.data)
 		return -1;
+
 	//create a matrix of the same size  and type as src (for dst)
 	dst.create(src.size(),src.type());
+
 	//convert src to gray scale (src_gray)
 	cvtColor(src,src_gray,CV_BGR2GRAY);
-	//create a window
-	namedWindow("Canny", CV_WINDOW_AUTOSIZE);
 
-	_structAsphaltInfo = FreeDrivingSpaceInfo(src_gray);
+	structAsphaltInfo _structAsphaltInfo = FreeDrivingSpaceInfo(src_gray);
 	cout<<"Median is: "<<_structAsphaltInfo.median<<endl;
+
 	return 0;
 
 }
