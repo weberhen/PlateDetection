498c362df931a9b32ac9215da8f46ddb1d651ccf
Henrique Weber <iqueweber@gmail.com>
2013-11-06 15:49:27 -0200

    missing files inserted at remote repo

---
+++ src/SquareDetection.hpp
@@ -0,0 +1,12 @@
+#ifndef SQUARE_DETECTION
+#define SQUARE_DETECTION
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <iostream>
+#include <math.h>
+#include <string.h>
+
+#endif
